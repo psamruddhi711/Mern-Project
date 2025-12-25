cdeca1e2e87e6e66c40a0a3df9477b989b7e03fa
Devyani Patil <devyanivpatil13@gmail.com>
2025-12-25 10:12:08 +0530

    updated code

--- server/server.js
+++ server/server.js
@@ -3,18 +3,23 @@ const cors = require("cors");
 //const {authUser} = require("./utils/auth");
 const userRouter = require("./routes/users");
 const adminRouter = require("./routes/admin");
-
-// const courseRoutes = require('./routes/course');
-// app.use('/course', courseRoutes);
+const courseRouter = require('./routes/course');
 const { authUser, checkAuthorization } = require("./utils/auth");
 const app = express();
 
 // middlewares
 app.use(express.json());
 app.use(authUser);
+<<<<<<< HEAD
 
 app.use("/admin",adminRouter)
 app.use(cors());
+=======
+app.use("/admin", adminRouter);
+app.use("/course", courseRouter);
+app.use(cors());
+
+>>>>>>> d4a2abd (Updated course routes, server config, and frontend pages)
 //app.use(checkAuthorization);
 app.use("/users", userRouter);
 
