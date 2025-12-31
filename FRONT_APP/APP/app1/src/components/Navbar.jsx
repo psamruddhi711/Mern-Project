2db4cabd6bb52fea4397c633fa6e3a32ee7c2c23
Ayush <ayushpachore2005@gmail.com>
2025-12-31 09:30:25 +0530

    nav

--- FRONT_APP/APP/app1/src/components/Navbar.jsx
+++ FRONT_APP/APP/app1/src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-<<<<<<< HEAD
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm py-2 sticky-top">
       <div className="container-fluid">
 
@@ -22,29 +21,6 @@ function Navbar() {
         </NavLink>
 
         {/* Mobile Toggle */}
-=======
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm py-1 sticky-top">
-
-      <div className="container-fluid">
-
-        {/* Brand: Logo + Text */}
- <NavLink to="/home" className="navbar-brand d-flex align-items-center gap-2">
-  <img
-    src="/learnsphere-logo.png"
-    alt="LearnSphere Logo"
-    style={{ height: "26px", width: "auto" }}
-  />
-</NavLink>
-
-
-
-        {/* Brand */}
-        <NavLink className="navbar-brand fw-bold fs-3 text-warning" to="/home">
-          🚀 Sunbeam
-        </NavLink>
-
-        {/* Mobile toggle */}
->>>>>>> 971fa1e316c018b6f1b5313cf39cbe9c21aeeb15
         <button
           className="navbar-toggler"
           type="button"
@@ -54,14 +30,10 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-<<<<<<< HEAD
         {/* Navbar Content */}
         <div className="collapse navbar-collapse" id="navbarNav">
 
           {/* Left Links */}
-=======
-          {/* Left links */}
->>>>>>> 971fa1e316c018b6f1b5313cf39cbe9c21aeeb15
           <ul className="navbar-nav ms-3">
             <li className="nav-item">
               <NavLink className="nav-link fw-semibold" to="/home">
@@ -82,7 +54,6 @@ function Navbar() {
             </li>
           </ul>
 
-<<<<<<< HEAD
           {/* Right Buttons */}
           <div className="ms-auto d-flex gap-2">
             <NavLink
@@ -96,15 +67,6 @@ function Navbar() {
               to="/login"
               className="btn btn-sm btn-warning px-3"
             >
-=======
-          {/* Right buttons */}
-          <div className="ms-auto d-flex align-items-center gap-2">
-            <NavLink to="/signup" className="btn btn-sm btn-outline-warning px-3">
-              Sign Up
-            </NavLink>
-
-            <NavLink to="/login" className="btn btn-sm btn-warning px-3">
->>>>>>> 971fa1e316c018b6f1b5313cf39cbe9c21aeeb15
               Login
             </NavLink>
           </div>
