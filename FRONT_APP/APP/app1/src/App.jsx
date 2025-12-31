e84bae1e647f22876bb7fd59f1cc046c8a713010
juneid3800-sys <juneidshaikh3800@gmail.com>
2026-01-01 02:21:00 +0530

    App.jsx

--- FRONT_APP/APP/app1/src/App.jsx
+++ FRONT_APP/APP/app1/src/App.jsx
@@ -6,6 +6,7 @@ import AdminCourses from "./pages/AdminCourses";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Register from "./pages/Register";
+import VideoPage from "./pages/Videos";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
       <Route path="/admin/courses" element={<AdminCourses />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/register/:courseId" element={<Register />} />
+      {/* <Route path="/register/:courseId" element={<Register />} /> */}
+      <Route path="/Videos" element={<VideoPage />} />
       <Route path="*" element={<h2>Page Not Found</h2>} />
     </Routes>
   );
